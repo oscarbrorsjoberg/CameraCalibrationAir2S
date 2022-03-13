e85a05d377077247320bb8431c861a4f3979852e
Oscar Sjoeberg <oscarbror.sjoberg@gmail.com>
2022-03-13 18:08:16 +0100

    camera rename

--- src/utils.cpp
+++ src/utils.cpp
@@ -16,39 +16,3 @@ void createKnownBoardDim(cv::Size brdSize, float sqrEdgeLength, std::vector<cv::
 	}
 }
 
-/* void getChessboardCorners(std::vector<cv::Mat> images, std::vector<std::vector<cv::Point2f>> &allCrnrs, bool showResult = false) */
-/* { */
-/* 	std::vector<cv::Point2f> pointBuf; */
-/* 	for(std::vector<cv::Mat>::iterator iter = images.begin(); */ 
-/* 			iter != images.end(); iter++){ */
-/* 		bool found = findChessboardCorners(*iter, cv::Size(9, 6), pointBuf, */ 
-/* 				cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_NORMALIZE_IMAGE); */
-
-/* 		if(found){ */
-/* 				allCrnrs.push_back(pointBuf); */
-/* 		} */
-/* 		if(showResult){ */
-/* 			drawChessboardCorners(*iter, cv::Size(9, 6), pointBuf, found); */
-/* 			cv::imshow("Looking for Corners", *iter); */
-/* 			cv::waitKey(0); */
-/* 		} */
-/* 	} */
-/* } */
-
-/* void cameraCalibration(std::vector<cv::Mat> calibrationImages, cv::Size boardSize, float squaredLength, cv::Mat& cameraMatrix, cv::Mat &distortionParams, cv::Mat &stdDevDistortionParams, cv::Mat &stdDeviationExtrinsics, cv::Mat &viewError) */
-/* { */
-/* 	std::vector<std::vector<cv::Point2f>> checkerboardImageSpacePoints; */
-/* 	getChessboardCorners(calibrationImages, checkerboardImageSpacePoints, false); */
-/* 	std::vector<std::vector<cv::Point3f>> worldSpaceCornerPoints(1); */
-
-/* 	worldSpaceCornerPoints.resize(checkerboardImageSpacePoints.size(), worldSpaceCornerPoints[0]); */
-
-/* 	std::vector<cv::Mat> rVectors, tVectors; */
-/* 	distortionParams = cv::Mat::zeros(14, 1, CV_64F); */
-
-/* 	calibrateCamera(worldSpaceCornerPoints, checkerboardImageSpacePoints, */ 
-/* 			boardSize, cameraMatrix, ditortionParams, rVectors, tVecotrs, */
-/* 			stdDevDistortionParams, stdDeviationExtrinsics, viewError */
-/* 			); */
-
-/* } */
