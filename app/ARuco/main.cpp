9eeb03d8686a45184502047600da1769dad25e0c
Oscar Sjoberg <oscarbror.sjoberg@gmail.com>
2023-07-20 00:22:09 +0200

    aruco some

--- app/ARuco/main.cpp
+++ app/ARuco/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cstdlib>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -6,19 +7,39 @@
 #include <opencv2/aruco.hpp>
 #include <opencv2/objdetect/aruco_dictionary.hpp>
 
-/* bool read_cmd_line(std::string &calib, */ 
-/* 									std::string &image) */
+#include <filesystem>
 
+
+namespace fs = std::filesystem;
+
+/* small bin to generate 10 markers from 6x6*/
 
 int main(int argc, char *argv[])
 {
-  cv::Mat markerImage;
 
+  if(argc < 2){
+    std::cout << "Please provide folder" << std::endl;
+    std::cout << "Usage ./bin/aruco <folder>" << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::string out(argv[1]);
+
+	assert(fs::is_directory(out));
+  fs::path outp{out};
+
+  cv::Mat markerImage;
   cv::aruco::Dictionary dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
 
-  cv::aruco::generateImageMarker(dict, 23, 200, markerImage, 1);
-
-  cv::imwrite("marker23.png", markerImage);
+  for(int i = 0; i < 10; ++i){
+    outp /= std::to_string(i) + ".png";
+    cv::aruco::generateImageMarker(dict, i, 200, markerImage);
+    if(!fs::exists(outp)){
+      cv::imwrite(outp.string(), markerImage);
+    }
+    else{
+      std::cout << outp << " already exists!\n";
+    }
+  }
 
   return EXIT_SUCCESS;
 }
